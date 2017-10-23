41a42087a4d735a51fceb5f3b2adb9ff86f75ff4
chooie <charlie.hebert92@gmail.com>
2017-10-23 12:05:17 -0600

    Pull out lint options

--- dev/lint.clj
+++ dev/lint.clj
@@ -3,33 +3,34 @@
    [clojure.pprint :refer [pprint]]
    [eastwood.lint :as eastwood]))
 
+(defonce lint-options
+  [:bad-arglists
+   :constant-test
+   :def-in-def
+   :deprecations
+   :keyword-typos
+   :local-shadows-var
+   :misplaced-docstrings
+   :no-ns-form-found
+   :non-clojure-file
+   :redefd-vars
+   :suspicious-expression
+   :suspicious-test
+   :unused-fn-args
+   :unused-locals
+   :unused-meta-on-macro
+   :unused-namespaces
+   :unused-private-vars
+   :unused-ret-vals
+   :wrong-arity
+   :wrong-ns-form
+   :wrong-pre-post
+   :wrong-tag])
+
 (defn lint []
   (let [lint-results (eastwood/lint {:source-paths ["src dev"]
                                      :test-paths ["src"]
-                                     :add-linters [
-                                                   :bad-arglists
-                                                   :constant-test
-                                                   :def-in-def
-                                                   :deprecations
-                                                   :keyword-typos
-                                                   :local-shadows-var
-                                                   :misplaced-docstrings
-                                                   :no-ns-form-found
-                                                   :non-clojure-file
-                                                   :redefd-vars
-                                                   :suspicious-expression
-                                                   :suspicious-test
-                                                   :unused-fn-args
-                                                   :unused-locals
-                                                   :unused-meta-on-macro
-                                                   :unused-namespaces
-                                                   :unused-private-vars
-                                                   :unused-ret-vals
-                                                   :wrong-arity
-                                                   :wrong-ns-form
-                                                   :wrong-pre-post
-                                                   :wrong-tag
-                                                   ]})
+                                     :add-linters lint-options})
         warnings (:warnings lint-results)
         errors (:err lint-results)]
     (println "Linting the code...")
