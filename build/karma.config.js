a5c9c43eabf67262f9dfa5cad431bcaa6f5a7bc7
chooie <charlie.hebert92@gmail.com>
2017-11-14 19:39:34 -0700

    Fix karma config to include the correct files

--- build/karma.config.js
+++ build/karma.config.js
@@ -1,20 +1,28 @@
-let rootPath = "../out/"
+let rootPath = "out/"
 module.exports = function(config) {
   config.set({
-    basePath: __dirname,
+    basePath: "..",
+    frameworks: ["cljs-test"],
+    client: {
+      args: ["my_app.frontend.test_runner.run-all-tests"],
+      captureConsole: true
+    },
     files: [
-      rootPath + "/goog/base.js",
+      rootPath + "goog/base.js",
+      rootPath + "js/main.js",
       {
-        pattern: rootPath + '/**/*.js',
+        pattern: rootPath + "my_app/**/*.js",
         included: false
       }
     ],
     port: 9876,
     colors: true,
+    autowatch: false,
 
-    // Level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
-    //                  config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_WARN
+    // Possible logging options:
+    // config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN ||
+    // config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_WARN,
+    singleRun: false
   });
 };
