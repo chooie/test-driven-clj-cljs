24b89381fb1f1cafa2529ab59005fcc7f6f19b5b
chooie <charlie.hebert92@gmail.com>
2017-11-27 12:00:08 -0700

    Add cider task

--- boot_tasks/my_app/boot_tasks/core.clj
+++ boot_tasks/my_app/boot_tasks/core.clj
@@ -60,3 +60,22 @@
     :source-map true
     :optimizations :none)
    (boot-tasks/target :dir #{"generated/development"})))
+
+(boot/deftask cider
+  "CIDER profile"
+  []
+  (require 'boot.repl)
+  (swap! @(resolve 'boot.repl/*default-dependencies*)
+         concat '[[org.clojure/tools.nrepl "0.2.13"]
+                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]
+                  [refactor-nrepl "2.4.0-SNAPSHOT"]])
+  (swap! @(resolve 'boot.repl/*default-middleware*)
+         concat '[cider.nrepl/cider-middleware
+                  refactor-nrepl.middleware/wrap-refactor])
+  identity)
+
+
+(boot/deftask start-cider-development-repl []
+  (comp
+   (cider)
+   (start-cljs-repl)))
