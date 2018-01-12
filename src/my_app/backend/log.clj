334b1416d65a02d05f87482228b396749248e6eb
chooie <charlie.hebert92@gmail.com>
2018-01-12 15:40:08 -0700

    Refactor log.clj to be a bit clearer

--- src/my_app/backend/log.clj
+++ src/my_app/backend/log.clj
@@ -7,14 +7,14 @@
 
 (defn is-valid-log-level?
   [possible-log-levels-set log-level]
-  (possible-log-levels-set log-level))
+  (contains? possible-log-levels-set log-level))
 
 (defn set-logging-level!
   [level-key]
-  (if ((complement is-valid-log-level?) log-levels level-key)
+  (if (not (is-valid-log-level? log-levels level-key))
     (error/throw-with-trace
-     (str "'" level-key "' is not a valid log level. Try one " "of: "
-          log-levels))
+     (str
+      "'" level-key "' is not a valid log level. Try one " "of: " log-levels))
     (timbre/merge-config! {:level level-key})))
 
 (defn info
