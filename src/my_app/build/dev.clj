8ddd486e67603feff910a10fd62941524a45c721
chooie <charlie.hebert92@gmail.com>
2018-01-25 16:35:12 -0700

    Remove test functions from dev namespace

--- src/my_app/build/dev.clj
+++ src/my_app/build/dev.clj
@@ -74,35 +74,5 @@
      "Reloaded namespaces after: "
      started-at)))
 
-(defn test-all
-  "Use this to check your work as you make changes"
-  []
-  (let [started-at (time-reporting/get-time-in-ms-now)]
-    (safe-refresh)
-    (check/lint-and-run-all-unit-tests)
-    (check/run-smoke-tests)
-    (time-reporting/measure-and-report-elapsed-time
-     "Build and check finished after: "
-     started-at)
-    (println "CHECK OK!")))
-
-(defn test-without-linting
-  []
-  (let [started-at (time-reporting/get-time-in-ms-now)]
-    (safe-refresh)
-    (check/run-all-unit-tests)
-    (check/run-smoke-tests)
-    (time-reporting/measure-and-report-elapsed-time
-     "Test without linting finished after: "
-     started-at)))
-
-(defn test-without-linting-and-smoke-tests
-  []
-  (let [started-at (time-reporting/get-time-in-ms-now)]
-    (check/run-all-unit-tests)
-    (time-reporting/measure-and-report-elapsed-time
-     "Test without linting finished after: "
-     started-at)))
-
 (defn start-cljs []
   (boot-cljs-repl/start-repl))
