abb641b8029a10d2f701c75d120afd0cd5a42d86
chooie <charlie.hebert92@gmail.com>
2018-01-30 01:18:15 -0700

    Get first css test working

--- src/my_app/common/css/core.cljc
+++ src/my_app/common/css/core.cljc
@@ -1,7 +1,11 @@
 (ns my-app.common.css.core
   (:require [garden.core :as garden]))
 
+(def light-green "rgb(144, 238, 144)")
+
 (defn generate-styles []
   (garden/css
    [:html
-    {:background-color "lightblue"}]))
+    {:background-color "lightblue"}]
+   [:body
+    {:background-color light-green}]))
--- src/my_app/frontend/core_test.cljs
+++ src/my_app/frontend/core_test.cljs
@@ -1,7 +1,19 @@
 (ns my-app.frontend.core-test
   (:require
-   [cljs.test :as test :include-macros true]))
+   [cljs.test :as test :include-macros true]
+   [my-app.common.css.core :as css]))
 
 (test/deftest core-test
   (test/testing "Number assertions"
     (test/is (false? (> 1 2)) "1 is smaller than 2")))
+
+(defn get-computed-style
+  [element property]
+  (let [styles (.getComputedStyle js/window element)
+        property-value (.getPropertyValue styles property)]
+    property-value))
+
+(test/deftest styles-test
+  (test/testing "Styles"
+    (test/is (= css/light-green
+                (get-computed-style (.-body js/document) "background-color")))))
